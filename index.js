9c88d6d1b7cad9dd22152297d6a14955e7511539
soniv ahmad <sonivahmad@gmail.com>
2022-07-08 23:54:23 +0530

    Js Ques

---
+++ index.js
@@ -0,0 +1,49 @@
+// Q1
+// function createArray(){
+//     let array=["Bike","Scooter","Car","Truck","JCB","Helicopter"];
+//     return array[3]
+//   }
+//   console.log(createArray())
+//   exports.createArray = createArray
+
+
+// Q2
+// function arrayHelloWorld(arr){
+//     return (arr.length > 5)?"Hello":"World"
+//   }
+//   console.log(arrayHelloWorld([1,2,3,4,5,6,7]))
+//   exports.arrayHelloWorld = arrayHelloWorld
+
+
+//   Q3
+//   function sumFirstLast(arr){
+//     return arr[0]+arr[arr.length -1]
+//   }
+//   console.log(sumFirstLast([1, 2, 3 ,4 ,5 ,6,7]))
+//   exports.sumFirstLast = sumFirstLast
+
+//   Q4
+//   function totalCandies(arr){
+//     let totalCandies=0
+//      for(let i=0;i<arr.Length;i++){
+//       totalCandies+= arr[i]
+     
+//       }
+//      return totalCandies
+//    }
+//    console.log(totalCandies([1, 3, 4, 7, 5]))
+//    exports.totalCandies = totalCandies
+
+//    Q5
+//    function arrayConcat(arr1, arr2){
+//     return arr1.concat(arr2)
+//   }
+//   console.log(arrayConcat([1, 3],[2, 5]))
+//   exports.arrayConcat = arrayConcat
+
+//   Q6
+//   function isArray(arr){
+//     return (Array.isArray(arr) == true)? 'YES' : 'NO'
+//  }
+//  console.log(isArray([1,2,3]))
+//  exports.isArray = isArray
